d1d22478f84e7c7438b1d5ec2e182cd22cb9e7f9
ankitmukhia <ankitmukhia157@gmail.com>
2024-05-03 13:36:55 +0530

    new problem solved

--- Typescript/JS-Practices/src/array-deff.ts
+++ Typescript/JS-Practices/src/array-deff.ts
@@ -1,5 +1,38 @@
-function arrayDiff() {
+// 1st solution
+
+// function arrayDiff(a: number[], b: number[]) {
+// 	// remove all value from which are matchable to a and visa versa
+// 	const set1 = new Set(a);
+// 	const set2 = new Set(b);
+//
+// 	const filterOut = new Set([...set1, ...set2]);
+//
+// 	const nonRepeatedNum = [...set1].filter(x => {
+// 		const isInSet1 = set2.has(x);
+// 		return !isInSet1
+// 	})
+//
+// 	return nonRepeatedNum
+// }
+
+// 2nd solution
+
+// function arrayDiff(a: number[], b: number[]) {
+// 	const nonRepeatedNum = a.filter(x => !b.includes(x));
+// 	return nonRepeatedNum
+// }
+//
+// 3rd solution
+//
+
+function arrayDiff(a: number[], b: number[]) {
+	const nonRepeatedNum1 = a.filter(x => !b.includes(x));
+
+	const nonRepeatedNum2 = b.filter(x => !a.includes(x));
+
+	const combinedVal = [...nonRepeatedNum1, ...nonRepeatedNum2]
+	return combinedVal
 }
 
-const caldiff = arrayDiff()
+const caldiff = arrayDiff([1, 8, 2], [])
 console.log(caldiff);
--- Typescript/JS-Practices/src/sort-order.ts
+++ Typescript/JS-Practices/src/sort-order.ts
@@ -1,5 +1,15 @@
 export const sort = (words: string) => {
 	const sString = words.split(" ")
+	//     If you get a positive number: The toy with the bigger number goes first.
+	//     If you get a negative number: The toy with the smaller number goes first.
+	//     If you get zero: Their order stays the same.
+	//
+	// So, if you have toys with numbers [3, 1, 5, 2] and you use this magic rule,     you'll compare them like this:
+	//
+	//     Comparing 3 and 1: 3 - 1 = 2 (positive), so 1 goes first.
+	//     Comparing 1 and 5: 1 - 5 = -4 (negative), so 1 stays first.
+	//     Comparing 5 and 2: 5 - 2 = 3 (positive), so 2 goes next.
+
 	sString.sort((a: any, b: any) => {
 		const charA = parseInt(a.match(/\d+/));
 		const charB = parseInt(b.match(/\d+/));
