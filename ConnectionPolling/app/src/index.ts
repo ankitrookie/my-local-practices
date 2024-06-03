9f11f9c95d38fd85f0c2c0fb9b0e151eebee1e27
ankitmukhia <ankitmukhia157@gmail.com>
2024-06-03 10:36:05 +0530

    bit changes

--- ConnectionPolling/app/src/index.ts
+++ ConnectionPolling/app/src/index.ts
@@ -1,4 +1,4 @@
-import { Hono, Next } from 'hono'
+import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { env } from 'hono/adapter'
